a09fad3ab01d363bfe0b997b0872ccd440396f38
Diones <pereiradiones987@gmail.com>
2023-02-22 17:17:34 -0300

    corrigindo erro de conexão com o mongodb

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const mongoose = require('mongoose')
 
 async function main() {
-  await mongoose.connect(`mongodb://localhost:27017/testedb`)
+  await mongoose.connect(`mongodb://127.0.0.1:27017/testedb`)
   console.log(chalk.bgGreenBright.black('Conetado ao MongoDB com sucesso'));
 }
 
